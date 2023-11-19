ec256ce68d001dbadf68d862651e401a09ce89e4
JS2IIU-MH <makoto.harazawa@gmail.com>
2023-11-19 23:53:43 +0900

    civ.py - base module for CI-V communication

---
+++ ci-v_gui.py
@@ -0,0 +1,250 @@
+''' CI-V interface monitor '''
+
+import tkinter as tk
+from tkinter import ttk
+import threading
+import time
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+import civ
+
+class Application(tk.Frame):
+    ''' GUI Application '''
+    def __init__(self, master) -> None:
+        super().__init__(master)
+        self.grid()
+
+        self.is_connected = False
+
+        self.scope_data_list = []
+        self.center_freq = 0
+        self.span = 0
+
+        self.flg_scope_run = False
+
+        self.rig_info_update_count = 0
+        
+
+        BUTTON_WIDTH_MID = 12
+        LABEL_WIDTH_MID = 12
+
+        frame1 = tk.Frame(master)
+        self.label_freq = tk.Label(frame1, text='--- Hz', width=20, font=('Calibri', 26, 'bold'))
+        self.label_freq.grid(row=0, column=0)
+        self.label_mode = tk.Label(frame1, text='MODE', width=10, font=('Calibri', 16, 'bold'))
+        self.label_mode.grid(row=0, column=1)
+        frame1.grid(pady=5)
+
+        frame2 = tk.Frame(master)
+        self.gen_graph(frame2)
+        frame2.grid(pady=5)
+
+        frame3 = tk.Frame(master)
+        self.button_scope_run = tk.Button(frame3, text='Scope Run', width=BUTTON_WIDTH_MID, font=('Calibri', 14, 'bold'), command=self.com_scope_run)
+        self.button_scope_run.grid(row=0, column=0)
+        self.button_scope_stop = tk.Button(frame3, text='Scope Stop', width=BUTTON_WIDTH_MID, font=('Calibri', 14, 'bold'), command=self.com_scope_stop)
+        self.button_scope_stop.grid(row=0, column=1)
+        self.button_save = tk.Button(frame3, text='Save', width=BUTTON_WIDTH_MID, font=('Calibri', 14, 'bold'))
+        self.button_save.grid(row=0, column=2)
+        frame3.grid(pady=5)
+
+        frame4 = tk.Frame(master)
+        self.label_vd = tk.Label(frame4, text='Vd', width=LABEL_WIDTH_MID, font=('Calibri', 14, 'bold'))
+        self.label_temp = tk.Label(frame4, text='Temp', width=LABEL_WIDTH_MID, font=('Calibri', 14, 'bold'))
+        button_rig_on = tk.Button(frame4, text='Rig On', width=BUTTON_WIDTH_MID, font=('Calibri', 14, 'bold'), command=self.com_rig_on)
+        button_rig_off = tk.Button(frame4, text='Rig Off', width=BUTTON_WIDTH_MID, font=('Calibri', 14, 'bold'), command=self.com_rig_off)
+        button_connect = tk.Button(frame4, text='Connect', width=BUTTON_WIDTH_MID, font=('Calibri', 14, 'bold'), command=self.com_connect)
+        combobox_com = ttk.Combobox(frame4, values=civ.CIV.serial_port_list(), height=3)
+        combobox_com.set('select com port')
+        
+        self.label_vd.grid(row=0, column=0)
+        combobox_com.grid(row=0, column=1)
+        button_connect.grid(row=0, column=2)
+        self.label_temp.grid(row=1, column=0)
+        button_rig_on.grid(row=1, column=1)
+        button_rig_off.grid(row=1, column=2)
+        frame4.grid(pady=5)
+
+        frame5 = tk.Frame(master)
+        button_exit = tk.Button(frame5, text='Exit', command=quit, width=BUTTON_WIDTH_MID, font=('Calibri', 14, 'bold'))
+        button_exit.grid()
+        frame5.grid(pady=5)
+
+        ##
+        # ci-v instance
+        self.my_rig = civ.CIV('COM5')
+        is_connected = True
+        time.sleep(1)
+        self.my_rig.stop_scope_readout()
+        self.rig_data_update()
+        time.sleep(1)
+
+        # threading
+        self.thread1 = threading.Thread(target=self.th_info_update, daemon=True)
+        self.thread1.start()
+
+        self.thread2 = threading.Thread(target=self.th_data_update, daemon=True)
+        self.thread2.start()
+
+        # frame1a = tk.Frame()
+        # self.gen_mpl_graph(frame1a)
+        # frame1a.grid()
+
+    def com_scope_run(self):
+        if self.flg_scope_run == False:
+            self.flg_scope_run = True
+
+    def com_scope_stop(self):
+        if self.flg_scope_run == True:
+            self.flg_scope_run = False
+
+    def com_save(self):
+        pass
+
+    def com_connect(self):
+        self.rig_data_update()
+
+    def com_rig_on(self):
+        self.my_rig.pwr_on()
+
+    def com_rig_off(self):
+        ''' shut down rig'''
+        self.my_rig.pwr_off()
+
+    def gen_graph(self, master):
+        ''' draw pectrum scope '''
+        self.fig, self.ax = plt.subplots()
+        self.fig.set_figwidth(5)
+        self.fig.set_figheight(2)
+
+        # dummy data for initial plot
+        dummy_x = np.linspace(0, 200, 475)
+        dummy_y = np.linspace(10, 150, 475)
+
+        (self.graph,) = self.ax.plot(dummy_x, dummy_y, linewidth=1)
+        # self.ax.set_xlabel('Freqency')
+        # ax.set_ylabel('Amplitude')
+        # ax.set_title('Spectrum Scope')
+        self.ax.grid()
+
+        # MatplotlibのFigureをTkinterのCanvasに埋め込む
+        canvas = FigureCanvasTkAgg(self.fig, master=master)
+        canvas_widget = canvas.get_tk_widget()
+        canvas_widget.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    
+    def redraw_graph(self, event=None):
+        # dummy
+        dummy_x = np.linspace(0, 200, 475)
+        dummy_y = np.linspace(30, 120, 475)
+
+        SCOPE_DATA_LENGTH = 475
+
+        if len(self.scope_data_list) == SCOPE_DATA_LENGTH:
+            x = np.linspace(self.center_freq - self.span, self.center_freq + self.span, SCOPE_DATA_LENGTH)
+            y = self.scope_data_list
+
+            self.graph.set_xdata(x)
+            self.graph.set_ydata(y)
+
+            self.ax.set_xlim(self.center_freq - self.span, self.center_freq + self.span)
+            self.ax.set_xticks(self.span_to_xticks(self.center_freq, self.span))
+            self.ax.set_xticklabels(self.span_to_xticklabels(self.span), fontsize=6)
+
+            self.fig.canvas.draw()
+            self.fig.canvas.flush_events()
+            time.sleep(0.1)
+
+    def rig_data_update(self):
+        # print('hh')
+        # freq
+        freq = self.my_rig.read_freq()
+        if freq != 0:
+            self.label_freq['text'] = f'{freq:,} Hz'
+
+        # mode
+        if self.my_rig.read_opmode() != 'N/A':
+            self.label_mode['text'] = self.my_rig.read_opmode()
+
+        # Vd
+        vd = self.my_rig.read_vd()
+        # print(vd)
+        if vd != 0:
+            self.label_vd['text'] = f'Vd: {vd:.3g} V'
+
+    def span_to_xticklabels(self, span):
+        NUM_XTICKS_LABEL = 11
+        span = span / 1000
+        xticks_list = np.linspace(start=-span, stop=span, num=NUM_XTICKS_LABEL)
+        
+        return xticks_list
+    
+    def span_to_xticks(self, center_freq, span):
+        NUM_XTICKS_LABEL = 11
+        tick_start = center_freq - span
+        tick_stop = center_freq + span
+
+        xticks_list = np.linspace(start=tick_start, stop=tick_stop, num=NUM_XTICKS_LABEL)
+        
+        return xticks_list
+
+    def th_info_update(self):
+        while True:
+            self.rig_data_update()
+            time.sleep(3)
+
+    def th_data_update(self):
+        RIG_INFO_INTERVAL = 3000
+
+        # start scope readout
+        self.my_rig.start_scope_readout()
+
+        while True:
+            # print(self.rig_info_update_count)
+            if self.flg_scope_run:
+                self.scope_data_list, self.center_freq, self.span = self.my_rig.read_spectrum(True)
+
+                # update graph
+                self.redraw_graph()
+                # time.sleep(0.5)
+
+                # freq update
+                if self.center_freq != 0:
+                    self.label_freq['text'] = f'{self.center_freq:,} Hz'
+            
+            if self.rig_info_update_count == RIG_INFO_INTERVAL:
+                # stop scope readout
+                self.my_rig.stop_scope_readout()
+                # rig info update
+                self.rig_data_update()
+                # start scope readout
+                self.my_rig.start_scope_readout()
+                self.rig_info_update_count = 0
+            else:
+                self.rig_info_update_count += 1
+    
+    def __del__(self):
+        ''' destructor '''
+        self.my_rig.stop_scope_readout()
+        # self.thread1.join()
+        self.thread2.join()
+
+        
+
+def main():
+    ''' main function generating window instance '''
+    root = tk.Tk()
+    root.title("CI-V Rig Monitor by JS2IIU")
+    root.geometry('550x500')
+    root.grid_anchor(tk.CENTER)
+
+    app = Application(master=root)
+
+    app.mainloop()
+
+
+if __name__ == '__main__':
+    main()
---
+++ civ.py
@@ -0,0 +1,393 @@
+'''basic example for CI-V programming '''
+import re
+import struct
+import time
+
+import serial
+from serial.tools import list_ports
+
+
+## Const for I-COM rigs
+ICOM_DRIVER_KW = 'CP210x'
+DATA_MAX = 11
+
+## CI-V commands
+PREAMBLE = [0xFE, 0xFE]
+POSTAMBLE = [0xFD]
+
+# ID-51
+# ADDR_RIG = [0x86]
+
+# IC-R6
+ADDR_RIG = [0x7E]
+
+# IC-7300
+# ADDR_RIG = [0x94]
+
+# PC HOST ADDRESS
+ADDR_HOST = [0x00]
+
+cmd_read_freq = [0x03]
+cmd_read_Smeter = [0x15, 0x02]
+cmd_read_spectrum = [0x27, 0x00]
+cmd_scope_on = [0x27, 0x10, 0x01]
+cmd_scope_off = [0x27, 0x10, 0x00]
+cmd_scope_readout_on = [0x27, 0x11, 0x01]
+cmd_scope_readout_off = [0x27, 0x11, 0x00]
+
+cmd_pwr_on = [0x18, 0x01]
+cmd_pwr_off = [0x18, 0x00]
+
+cmd_temp = []
+cmd_vd = [0x15, 0x15]
+
+cmd_read_opmode = [0x04]
+
+
+class CIV():
+    ''' class CIV, controls i-com rig via CI-V
+        direct serial connection or CI-V intercface is necessary
+    '''
+    def __init__(self, com_port) -> None:
+
+        # regex pattern for scope data read out
+        # only for IC-7300
+        if ADDR_RIG == [0x94]:
+            self.pat_01 = re.compile(r'fefe0094270000([0-9]{2})([0-9]{2})([0-9]+)fd')
+            self.pat_02 = re.compile(r'fefe0094270000([0-9]{2})([0-9]{2})(\w{50,100})fd')
+            # pat_11 = re.compile(r'fefe0094270000([0-9]{2})([0-9]{2})(\w{50})fd')
+            self.pat_all = re.compile(r'fefe0094270000([0-9]{2})([0-9]{2})(\w{24}|\w{50}|\w{100})fd')
+
+        self.ser = serial.Serial(port=com_port,
+                                 # baudrate=115200,
+                                 baudrate=19200,
+                                 parity=serial.PARITY_NONE,
+                                 stopbits=serial.STOPBITS_ONE,
+                                 timeout=2)
+        # self.is_icom_rig = self.check_port()
+
+    def check_port(self):
+        ''' check port is for i-com rigs '''
+        is_found = False
+
+        ports = list_ports.comports()
+        for po in ports:
+            res1 = re.findall(ICOM_DRIVER_KW, po.description)
+            res2 = re.findall(self.ser.name, po.description)
+            if res1 and res2:
+                is_found = True
+
+        return is_found
+
+    def send_msg(self, command_list):
+        ''' send_msg to rig
+            Args:
+                command_list, byte-by-byte command in list format
+                command_list should include preamble, address and postamble
+                ex) [0xFE, 0xFE, 0x94, ...., 0xFD]
+            Returns:
+                raw buffer bytes content
+        '''
+        msg = b''
+        for cmd in command_list:
+            msg = msg + struct.pack('B', cmd)
+        self.ser.write(msg)
+
+        self.ser.flush()
+        time.sleep(0.2)
+
+        buffer = self.ser.readline()
+        return buffer
+    
+    def read_msg(self):
+        # TODO: readlineのほうが良いかも
+        # buffer = self.ser.read(100)
+        buffer = self.ser.readline()
+        print(buffer)
+        return buffer
+
+    def pwr_off(self):
+        ''' shut down '''
+        msg_list = PREAMBLE + ADDR_RIG + ADDR_HOST + cmd_pwr_off + POSTAMBLE
+        ret = self.send_msg(msg_list)
+    
+    def pwr_on(self):
+        ''' pwr on - does not work on USB connection '''
+        # baud 19200 -> 0xFE * 25
+        NUM_RPT = 25
+        WAKE = [0xFE]
+        for _ in range(NUM_RPT):
+            ret = self.send_msg(WAKE)
+        msg_list = PREAMBLE + ADDR_RIG + ADDR_HOST + cmd_pwr_on + POSTAMBLE
+        ret = self.send_msg(msg_list)
+        
+
+    def read_freq(self):
+        ''' Returns Frequency in Hz '''
+        msg_list = PREAMBLE + ADDR_RIG + ADDR_HOST + cmd_read_freq + POSTAMBLE
+        ret = self.send_msg(msg_list)
+        ret_s = ''
+        # TODO: アドレスごとに分岐させて処理するのはやめたい
+        if ADDR_RIG == [0x94]:
+            s = re.search(r'fefe009403([0-9]{10})fd', ret.hex())
+        elif ADDR_RIG == [0x86]:
+            s = re.search(r'fefe008603([0-9]{10})fd', ret.hex())
+        elif ADDR_RIG == [0x7E]:
+            s = re.search(r'fefe007e03([0-9]{10})fd', ret.hex())
+        if s is not None:
+            ret_s = s.group(1)
+            # print(len(ret))
+        if len(ret_s) == 10:
+            # 送信したメッセージから0xfdの2文字分減らして帰ってきた文字のみ抽出する=周波数の文字列
+            # ret = ret[(len(msg_list) - 1) * 2:-2]
+            freq = self.reverse_msg(ret_s)
+            freq = int(freq)
+            # print(freq)
+        else:
+            freq = 0
+
+        return freq
+
+    def stop_scope_readout(self):
+        # scope readout stop
+        msg_list = PREAMBLE + ADDR_RIG + ADDR_HOST + cmd_scope_readout_off + POSTAMBLE
+        ret = self.send_msg(msg_list)
+    
+    def start_scope_readout(self):
+        # scope readout stop
+        msg_list = PREAMBLE + ADDR_RIG + ADDR_HOST + cmd_scope_readout_on + POSTAMBLE
+        ret = self.send_msg(msg_list)
+
+    def read_spectrum(self, is_1st=False):
+        ''' read out spectrum scope data from IC-7300
+            Args:
+                is_1st: if 1st time run, set True
+            Returns:
+                scope_data_list: data as int
+                center_freq: scope center frequency in Hz
+                span: frequency span in Hz
+        '''
+        count = 0
+        ret_dat = bytes(0)
+
+        # if sspectrum scope is not shown in rig's display, turn on scope
+        if is_1st:
+            # scope on
+            msg_list = PREAMBLE + ADDR_RIG + ADDR_HOST + cmd_scope_on + POSTAMBLE
+            ret = self.send_msg(msg_list)
+            # scope readout on
+            msg_list = PREAMBLE + ADDR_RIG + ADDR_HOST + cmd_scope_readout_on + POSTAMBLE
+            ret = self.send_msg(msg_list)
+
+            # read spectrum data
+            msg_list = PREAMBLE + ADDR_RIG + ADDR_HOST + cmd_read_spectrum + POSTAMBLE
+            ret_dat = self.send_msg(msg_list)
+        # print(ret.hex())
+        while count < 15:
+            ret_dat = ret_dat + self.ser.readline()
+            count += 1
+
+        # find read out response
+        # ACK: fefe0094 27 00 00 fa fd
+        # data1: fefe0094 27 00 00 01 11 00 ********** 00 ********** fd
+        # data2-11: fefe0094 27 00 ## 11 **data(50/100)** fd
+        #
+        # regex pattern definition in self.__init__()
+        res = re.findall(self.pat_all, ret_dat.hex())
+        # res_data = re.findall(self.pat_02, ret_dat.hex())
+
+        count = 1
+        scope_data = ''
+        scope_data_list = []
+        center_freq = 0
+        span = 0
+
+        is_find_1st = False
+        is_complete = False
+
+        for d in res:
+            # print(d[0])
+            if is_complete == False:
+                if is_find_1st == False:
+                    if d[0] == '01':
+                        # ### check freq/span
+                        data = d[2]
+                        center_freq = self.decode_freq(data[2:12])
+                        # print(center_freq, data[2:12])
+                        span = self.decode_span(data[12:])
+                        # print(f'centfreq: {center_freq:,} Hz, span: {span:,} Hz')
+                        
+                        is_find_1st = True
+                else:
+                    # ### add data
+                    scope_data = scope_data + d[2]
+                    if d[0] == '11':
+                        is_find_1st = False
+                        data_list = self.scope_data_to_list(scope_data)
+                        for d in data_list:
+                            scope_data_list.append(int(d,16))
+                        is_complete = True
+
+        return scope_data_list, center_freq, span
+
+
+    @classmethod
+    def scope_data_to_list(cls, scope_data):
+        SCOPE_DATA_LENGTH = 475 * 2
+        out_data = []
+        if len(scope_data) == SCOPE_DATA_LENGTH:
+            for i in range(SCOPE_DATA_LENGTH // 2):
+                out_data.append(scope_data[i*2:i*2+2])
+        
+        return out_data
+
+    @classmethod
+    def decode_freq(cls, freq_string):
+        FREQ_FULL_LENGTH = 10
+        FREQ_LENGTH = 10
+        if len(freq_string) == FREQ_FULL_LENGTH:
+            freq = ''
+            tmp = []
+            # 最初の2文字を捨てる
+            # freq_string = freq_string[2:]
+            for i in range(FREQ_LENGTH // 2):
+                tmp.append(freq_string[2*i:2*i+2])
+            for i in range(FREQ_LENGTH // 2):
+                freq = freq + tmp[FREQ_LENGTH // 2 - i - 1]
+        else:
+            freq = 0
+
+        return int(freq)
+
+    @classmethod
+    def decode_span(cls, span_string):
+        ''' decode span string, returns span in Hz'''
+        SPAN_FULL_LENGTH = 12
+        SPAN_LENGTH = 10
+        if len(span_string) == SPAN_FULL_LENGTH:
+            span = int(span_string[2:3]) * 1000\
+            + int(span_string[3:4]) * 100\
+            + int(span_string[4:5]) * 100000\
+            + int(span_string[5:6]) * 10000
+
+        else:
+            span = 0
+        return span
+
+    @classmethod
+    def serial_port_list(cls):
+        port_list = []
+        ports = list(serial.tools.list_ports.comports())
+        for p in ports:
+            # if 'USB Serial Port' in p.description:
+            port_list.append(p.device)
+        
+        return port_list
+
+    
+    def read_spectrum_to_file(self):
+        READ_MAX = 11
+        filename = 'spectrum_out2.txt'
+        count = 0
+
+        # scope on
+        msg_list = PREAMBLE + ADDR_RIG + ADDR_HOST + cmd_scope_on + POSTAMBLE
+        _ = self.send_msg(msg_list)
+
+        # scope readout on
+        msg_list = PREAMBLE + ADDR_RIG + ADDR_HOST + cmd_scope_readout_on + POSTAMBLE
+        _ = self.send_msg(msg_list)
+
+        with open(filename, 'w') as f:
+            # data request
+            msg_list = PREAMBLE + ADDR_RIG + ADDR_HOST + cmd_read_spectrum + POSTAMBLE
+            ret = self.send_msg(msg_list)
+            f.write(ret.hex() + '\n')
+
+            while count < 10:
+                ret = self.ser.readline()
+                f.write(ret.hex())
+                # print(f'#{count:02} {ret.hex()}')
+                count += 1
+    
+    def read_temp(self):
+        msg_list = PREAMBLE + ADDR_RIG + ADDR_HOST + cmd_temp + POSTAMBLE
+        ret = self.send_msg(msg_list)
+
+    def read_vd(self):
+        """ read Vd value in V, IC-7300 only"""
+        msg_list = PREAMBLE + ADDR_RIG + ADDR_HOST + cmd_vd + POSTAMBLE
+        ret = self.send_msg(msg_list)
+        s = re.search(r'fefe00941515([0-9]{4})fd', ret.hex())
+        if s is not None:
+            val = int(s.group(1))
+            # print(val)
+        else:
+            val = -1
+
+        if val < 0 or val > 255:
+            out_val = 0.0
+        elif val < 14:
+            out_val = 10 / 13 * val
+        else:
+            out_val = 6 / 228 * val + 10
+        
+        return out_val
+
+    def read_opmode(self):
+        ''' returnd mode in string'''
+        opmode_str = ['LSB', 'USB', 'AM', 'CW', 'RTTY', 'FM', 'Reserved', 'CW-R', 'RTTY-R', 'N/A']
+
+        msg_list = PREAMBLE + ADDR_RIG + ADDR_HOST + cmd_read_opmode + POSTAMBLE
+        ret = self.send_msg(msg_list)
+        s = re.search(r'fefe009404([0-9]{4})fd', ret.hex())
+        
+        if s is not None:
+            print(s.group(1))
+            num = int(s.group(1)[:2])
+        else:
+            num = 9
+        
+        # print(f'RO{opmode_str[num]}: {num}, {ret.hex()}')
+
+        return opmode_str[num]
+
+    @classmethod
+    def reverse_msg(cls, msg_in):
+        # 2桁ずつ、逆順に並んでいるので入れ替える
+        num = len(msg_in)
+        out_msg = ''
+        if num % 2 == 0:
+            for i in range(int(num / 2)):
+                out_msg = out_msg + msg_in[num-i*2-2:num-i*2]
+            return out_msg
+        else:
+            return ''
+
+    def __del__(self):
+        try:
+            self.ser.close()
+        except AttributeError:
+            pass
+
+
+
+def main():
+    ''' main func for test purpose '''
+    civ = CIV('COM5')
+
+    # リグに表示されている周波数[Hz] を取得する。
+    print(f'Frequency: {civ.read_freq():,} Hz')
+    # civ.pwr_off()
+    # l, cf, sp = civ.read_spectrum(True)
+    # print(cf, sp)
+    # print(l)
+
+    # civ.read_vd()
+    # civ.read_freq()
+    # print(civ.read_opmode())
+    # print(civ.serial_port_list())
+    
+
+if __name__ == '__main__':
+    main()
